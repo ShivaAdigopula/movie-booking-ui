1ffe2a8d957f9d26f02029c7cf41eea0dc97e112
ShivaAdigopula <vskrao.adigopula@gmail.com>
2022-04-16 16:04:27 +0400

    updated ManageBooking link to use navigate functionality

--- src/Components/AppBar.jsx
+++ src/Components/AppBar.jsx
@@ -1,19 +1,21 @@
 
 import * as React from 'react';
 import { AppBar, Box, Button, Link, Toolbar } from '@material-ui/core';
+import { useNavigate } from 'react-router-dom';
 
 
 export function ApplicationBar() {
+  const navigate = useNavigate();
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
 
 
-          <Button ><Link className="link" href="/" >
+          <Button ><Link className="link" to="/" >
             Home
   </Link></Button>
-          <Button><Link className="link" href="/manage-bookings" >
+          <Button><Link className="link" onClick={() => navigate('/manage-bookings')} >
             Manage Booking
   </Link></Button>
 
